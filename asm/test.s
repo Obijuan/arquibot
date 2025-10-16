37ddfc3beae06fc19b8e97012bd89803a63db797
obijuan <obijuan.cube@gmail.com>
2025-10-16 11:26:33 +0200

    add asm example

---
+++ asm/test.s
@@ -0,0 +1,10 @@
+
+    .data
+    .word 1
+    .word 2
+    .word 3
+
+    .text
+
+    li a7, 10
+    ecall
--- run-test.py
+++ run-test.py
@@ -7,13 +7,13 @@ from arquibot.rars import Rars
 
 # -- Valores esperados para las variables
 data_ok = {
-    "a": 10,
-    "b": 20,
-    "f": 36
+    "a": 1,
+    "b": 2,
+    "f": 3
 }
 
 # -- Preparar el contexto
-Rars("../calculo.s", bonus=10)
+Rars("asm/test.s", bonus=10)
 
 Rars.check_variables(data_ok)
 
