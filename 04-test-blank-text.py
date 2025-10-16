56ba28bddc8f60fc87908015df70494ca1fda876
obijuan <obijuan.cube@gmail.com>
2025-10-16 19:46:23 +0200

    Rars: add 04-test

---
+++ 04-test-blank-text.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from arquibot.rars import Rars
+
+# ───────────────────────────────────────
+# ─── TEST 4: Fichero asm con segmento
+# ─── de codigo en blanco
+# ─── (es equivalente a si no lo hubiese)
+# ───────────────────────────────────────
+
+# ── Comenzar la comprobación
+Rars("asm/blank-text.s")
+
+# ── Terminar
+Rars.exit()
