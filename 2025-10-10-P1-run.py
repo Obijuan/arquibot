dbe28d1853e8da3b7783ec8440c1c36dc2ef6ae8
obijuan <obijuan.cube@gmail.com>
2025-10-16 11:40:16 +0200

    Add run script for the exam 2025-10-10-P1-AC-Teleco

---
+++ 2025-10-10-P1-run.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from arquibot.rars import Rars
+
+# ───────────────────────────────────────
+#    MAIN
+# ───────────────────────────────────────
+
+# -- Valores esperados para las variables
+data_ok = {
+    "a": 10,
+    "b": 20,
+    "f": 36
+}
+
+# -- Preparar el contexto
+Rars("asm/2025-10-10-P1-AC-Teleco-calculo-sol.s", bonus=10)
+
+Rars.check_variables(data_ok)
+
+# -- Terminar
+Rars.exit()
