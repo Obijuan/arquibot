f82f4d1f07a1cfd83f7a837b858507da583fed1f
obijuan <obijuan.cube@gmail.com>
2025-10-16 19:40:59 +0200

    Rars: add 03-test

---
+++ 03-test-blank-data.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from arquibot.rars import Rars
+
+# ───────────────────────────────────────
+# ─── TEST 2: Fichero asm en blanco
+# ─── No hay segmento de codigo
+# ─── No hay segmento de datos
+# ───────────────────────────────────────
+
+# ── Comenzar la comprobación
+Rars("asm/blank-data.s", expected_data=True)
+
+# ── Terminar
+Rars.exit()
