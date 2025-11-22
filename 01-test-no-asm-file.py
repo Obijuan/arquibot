4b5542bb7cdbd65e6a97dc973671e29cb93683c6
obijuan <obijuan.cube@gmail.com>
2025-11-22 13:53:04 +0100

    Rars: Clean up tests

--- 01-test-no-asm-file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-from arquibot.rars import Rars
-
-# ───────────────────────────────────────
-# ─── TEST 1: Fichero asm incorrecto
-# ───────────────────────────────────────
-
-# ── Comenzar la comprobación
-Rars("asm/wrong-file.s")
---
+++ run-test-all.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python3
+from arquibot.rars import Rars
+
+# ───────────────────────────────────────
+#    MAIN
+# ───────────────────────────────────────
+
+# ── Comenzar la comprobación
+print("═════════════ TEST 1 ════════════════════════")
+test1 = Rars("asm/wrong-file.s")
+
+print("═════════════ TEST 2 ════════════════════════")
+test2 = Rars("asm/test-blank.s")
+
+print("═════════════ TEST 3 ════════════════════════")
+test3 = Rars("asm/test-blank-data.s", expected_data=True)
+
+print("═════════════ TEST 4 ════════════════════════")
+test4 = Rars("asm/test-blank-text.s")
+
+print("═════════════ TEST 5 ════════════════════════")
+test5 = Rars("asm/test-data-1word.s", expected_data=True)
+
+print("═════════════ TEST 6 ════════════════════════")
+test6 = Rars("asm/test-data-syntax-error.s", expected_data=True)
+
+print("═════════════ TEST 7 ════════════════════════")
+test7 = Rars("asm/test-data-syntax-error2.s", expected_data=True)
+
+print("═════════════ TEST 8 ════════════════════════")
+test8 = Rars("asm/test-data-syntax-error3.s", expected_data=True)
+
+print("═════════════ TEST 9 ════════════════════════")
+test9 = Rars("asm/test-text-1inst.s", expected_data=True)
+test9.exit()
+
+print("═════════════ TEST 10 ════════════════════════")
+test10 = Rars("asm/test-text-exit.s", expected_data=True)
+test10.exit()
+
+print("═════════════ TEST 11 ════════════════════════")
+test11 = Rars("asm/test-text-syntax-error.s", expected_data=True)
+test11.exit()
+
+print("═════════════ TEST 12 ════════════════════════")
+test12 = Rars("asm/test-variables.s", expected_data=True)
+
+# ── Valores esperados para las variables
+data_ok = {
+    "a": 1,
+    "b": 2,
+    "f": 3
+}
+# ── Comprobar los valores de las variables
+test12.check_variables(data_ok)
+test12.exit()
+
+print("═════════════ TEST 13 ════════════════════════")
+# ── Comenzar la comprobación
+test13 = Rars("asm/test-consola-out1.s", expected_data=True)
+
+# ── Mostrar la salida generada por el programa
+test13.show_console_output()
+
+# ── Terminar
+test13.exit()
+
+print("═════════════ TEST 14 ════════════════════════")
+# ── Comenzar la comprobación
+test14 = Rars("asm/test-consola-out1.s", expected_data=True)
+
+# ── Salida esperada para la consola
+# ── Es la salida correcta
+salidas_esperadas = [
+    "Test..."
+]
+
+# ── Comprobar la salida de la consola
+test14.check_console_output(salidas_esperadas)
+
+# ── Terminar
+test14.exit()
+
+
+print("═════════════ TEST 15 ════════════════════════")
+# ── Comenzar la comprobación
+test15 = Rars("asm/test-consola-out1.s", expected_data=True)
+
+# ── Salida esperada para la consola
+# ── Es una salida INCORRECTA
+salidas_esperadas = [
+    "Test...."
+]
+
+# ── Comprobar la salida de la consola
+test15.check_console_output(salidas_esperadas)
+
+# ── Terminar
+test15.exit()
+
+print("═════════════ TEST 16 ════════════════════════")
+test16 = Rars("asm/test-consola-out2.s", expected_data=True)
+
+salidas_esperadas = [
+    "Test...",
+    "Test...\n"
+]
+
+# ── Comprobar la salida de la consola
+test16.check_console_output(salidas_esperadas)
+
+# ── Terminar
+test16.exit()
+
+print("═════════════ TEST 17 ════════════════════════")
+# -- Preparar el contexto
+test17 = Rars(
+            "asm/test-string1.s",  # -- Main
+            expected_data=True,    # -- Segmento de datos
+          )
+
+# -- Valor esperado para la cadena
+CAD0_ESPERADA = "Test..."
+
+Rars.print_section("Comprobando cadenas")
+
+# -- Comprobar cadena destino
+test17.check_string(0, CAD0_ESPERADA, var_name="Cadena 0")
+
+# -- Terminar
+test17.exit()
+
+print("═════════════ TEST 18 ════════════════════════")
+test18 = Rars(
+          "asm/test-string2.s",  # -- Main
+          expected_data=True,    # -- Segmento de datos
+      )
+
+# -- Valores esperados para las cadena
+CAD0_ESPERADA = "Cadena 0"
+CAD1_ESPERADA = "Cadena 1"
+
+Rars.print_section("Comprobando cadenas")
+
+# -- Comprobar cadenas
+# -- Se pasa el offset y el valor esperado
+test18.check_string(0, CAD0_ESPERADA, var_name="Cad0")
+test18.check_string(9, CAD1_ESPERADA, var_name="Cad1")
+
+# -- Terminar
+test18.exit()
+
+
+print("═════════════ TEST 19 ════════════════════════")
+test19 = Rars(
+         "asm/test-string3.s",  # -- Main
+         expected_data=True,    # -- Segmento de datos
+       )
+
+Rars.print_section("Comprobando cadenas")
+
+# -- Valores esperados para las cadena
+CAD0_ESPERADA = "Cadena 0"
+CAD1_ESPERADA = "Cadena 1"
+CAD2_ESPERADA = "Cadena Test2"
+
+# -- Comprobar cadenas
+# -- Se pasa el offset y el valor esperado
+test19.check_string(0, CAD0_ESPERADA, var_name="Cad0")
+test19.check_string(9, CAD1_ESPERADA, var_name="Cad1")
+test19.check_string(18, CAD2_ESPERADA, var_name="Cad2")
+
+# -- Terminar
+test19.exit()
+
+
+print("═════════════ TEST 20 ════════════════════════")
+test20 = Rars(
+            "asm/test-include1.s",  # -- Main
+            "asm/servicios.s",  # -- Include
+       )
+
+# -- Terminar
+test20.exit()
+
+
+print("═════════════ TEST 21 ════════════════════════")
+# -- Se especifica un fichero include QUE NO EXISTE!
+test21 = Rars(
+            "asm/test-include1.s",    # -- Main
+            "asm/servicios-error.s",  # -- Include
+         )
+
+# -- Terminar
+test21.exit()
+
+
+print("═════════════ TEST 22 ════════════════════════")
+test = Rars(
+         "asm/test-input1.s",  # -- Main
+         "asm/servicios.s",    # -- Include
+
+         # -- Simular texto introducido por el usuario
+         input="Test"
+       )
+
+# -- Comprobar si el texto introducido esta en la memoria
+test.check_string(0, "Test\n", var_name="cad")
+
+# -- Terminar
+test.exit()
